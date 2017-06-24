d4c8320636ef9a7d35e4643853ad403d9886d10a
123\Huy <myheaven291@gmail.com>
2017-06-24 15:40:05 +0700

    .

---
+++ app/src/main/java/com/example/huy/storyy/activities/DescriptiontActivity.java
@@ -0,0 +1,15 @@
+package com.example.huy.storyy.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.huy.storyy.R;
+
+public class DescriptiontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descriptiont);
+    }
+}
